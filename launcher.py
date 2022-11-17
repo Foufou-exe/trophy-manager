4917ad80da36826b2f5717c15dec9637f99a7e1e
Dylan LLODRA <dylanllodrapro@gmail.com>
2022-11-17 10:24:04 +0100

    add file

--- launcher.py
+++ launcher.py
@@ -1,8 +1,21 @@
 # Importation des modules
-import tkinter
+from tkinter import *
 
-# Création de la fenêtre principale
-window = tkinter.Tk()
-window.title("Thophe's Launcher")
+# Création de la fenêtre principale 
 
+"""
+    Paramètrage de la Fenetre Principale
+"""
+
+window = Tk(className='Python Examples - Window Color')
+window.title("Trophe's Launcher")
+window.minsize(width=250, height=250)
+window.geometry("1280x720")
+window['background']='#856ff8'
+
+# Ajout d'un bouton
+
+#Window_Button = Button(window, tes)
+
+# Affichage de la fenetre
 window.mainloop()
---
+++ main.py
@@ -0,0 +1,6 @@
+from tkinter import *
+
+parent_obj = Tk()
+parent_obj.minsize(width=250, height=250)
+parent_obj.geometry("250x250")
+parent_obj.mainloop()
